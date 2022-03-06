20855573dd00f275b937e3f5777e48ca6c137332
thanhtoan1742 <thanhtoan1742@gmail.com>
2022-03-07 01:12:44 +0700

    new things

--- moblile/Assets/Scripts/ButtonCard.cs
+++ moblile/Assets/Scripts/ButtonCard.cs
@@ -1,24 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ButtonCard : MonoBehaviour
 {
+    public bool isOn = false;
+
     public GameObject slider;
     protected RectTransform sliderRectTransform;
-    public bool isOn = false;
+    private Task tweenCompleted = null;
+
+
+    private Vector2 GetPosition(bool isOn)
+    {
+        var x = sliderRectTransform.anchoredPosition.x;
+        x = Math.Abs(x);
+        var y = sliderRectTransform.anchoredPosition.y;
+        return new Vector2(isOn ? x : -x, y);
+    }
 
     void Awake()
     {
         sliderRectTransform = slider.GetComponent<RectTransform>();
+        sliderRectTransform.anchoredPosition = GetPosition(isOn);
     }
 
-    public void Toggle()
+    public async void Toggle()
     {
-        var x = sliderRectTransform.anchoredPosition.x;
-        var y = sliderRectTransform.anchoredPosition.y;
-        sliderRectTransform.anchoredPosition = new Vector2(-x, y);
+        if (tweenCompleted != null)
+        {
+            await tweenCompleted;
+        }
+
         isOn = !isOn;
+        tweenCompleted = DOTween.To(
+            () => sliderRectTransform.anchoredPosition,
+            pos => sliderRectTransform.anchoredPosition = pos,
+            GetPosition(isOn),
+            (float)(0.25)
+        ).AsyncWaitForCompletion();
     }
 }
---
+++ moblile/Assets/Scripts/ToggleSwitch.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ToggleSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [Header("Colors")]
+    public Color ativeColor;
+    public Color inactiveColor;
+
+    [Header("Animation duration")]
+    public float animationDuration = 0.5f;
+
+
+
+    protected GameObject leftEnd;
+    protected GameObject middle;
+    protected GameObject rightEnd;
+    protected GameObject knob;
+
+
+    protected RectTransform knobRectTransform;
+    protected float middleWidth;
+    protected Toggle toggle;
+
+    private List<Task> tweenTasks = new List<Task>();
+
+    protected void AssignChildGameObject()
+    {
+        leftEnd = gameObject.transform.GetChild(0).gameObject;
+        middle = gameObject.transform.GetChild(1).gameObject;
+        rightEnd = gameObject.transform.GetChild(2).gameObject;
+        knob = gameObject.transform.GetChild(3).gameObject;
+    }
+
+    void Awake()
+    {
+        AssignChildGameObject();
+        knobRectTransform = knob.GetComponent<RectTransform>();
+        toggle = gameObject.GetComponent<Toggle>();
+        middleWidth =
+            gameObject.GetComponent<RectTransform>().rect.width
+            - leftEnd.GetComponent<RectTransform>().rect.width
+            - rightEnd.GetComponent<RectTransform>().rect.width;
+
+        var middleRectTransform = middle.GetComponent<RectTransform>();
+        middleRectTransform.sizeDelta = new Vector2(
+            middleRectTransform.sizeDelta.x + middleWidth - middleRectTransform.rect.width,
+            middleRectTransform.sizeDelta.y
+        );
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        Task.WaitAll(tweenTasks.ToArray());
+        tweenTasks.Clear();
+
+        var pos = new Vector2(
+            toggle.isOn ? middleWidth/2 : -middleWidth/2,
+            knobRectTransform.anchoredPosition.y
+        );
+        tweenTasks.Add(DOTween.To(
+            () => knobRectTransform.anchoredPosition,
+            x => knobRectTransform.anchoredPosition = x,
+            pos,
+            animationDuration
+        ).AsyncWaitForCompletion());
+
+        foreach (var g in new GameObject[]{leftEnd, middle, rightEnd})
+        {
+            tweenTasks.Add(DOTween.To(
+                () => g.GetComponent<Image>().color,
+                x => g.GetComponent<Image>().color = x,
+                toggle.isOn ? ativeColor : inactiveColor,
+                animationDuration
+            ).AsyncWaitForCompletion());
+        }
+    }
+}
