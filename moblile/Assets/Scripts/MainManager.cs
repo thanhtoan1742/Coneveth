33629fca3701645a3c467da2cf43d482480cfd71
thanhtoan1742 <thanhtoan1742@gmail.com>
2022-03-19 21:01:28 +0700

    mqtt

--- moblile/Assets/Scripts/MainManager.cs
+++ moblile/Assets/Scripts/MainManager.cs
@@ -15,48 +15,65 @@ public class MainManager : MonoBehaviour
     public GameObject usernameText;
     public GameObject passwordText;
 
+    [Header("Input and Error")]
+    public GameObject loginForm;
+    public GameObject error;
+
 
     protected MqttClientBehavior mqttClientBehavior;
 
-    protected void Awake()
-    {
-        if (instance != null)
-        {
+    protected void Awake() {
+        if (instance != null) {
             Destroy(gameObject);
             return;
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
         mqttClientBehavior = gameObject.GetComponent<MqttClientBehavior>();
+        mqttClientBehavior.ConnectionSucceeded += mqttOnConnectionSucceeded;
+        mqttClientBehavior.ConnectionFailed += mqttOnConnectionFailed;
     }
 
-    public void SubscribeTopic(string topic, Action<string> callback)
-    {
-        mqttClientBehavior.AddSubscribedTopicCallbacks(topic, callback);
+    public void SubscribeTopic(string topic, Action<string> callback) {
+        mqttClientBehavior.AddListenter(topic, callback);
     }
 
-    public void PublishTopic(string topic, string message)
-    {
+    public void PublishTopic(string topic, string message) {
         mqttClientBehavior.Publish(topic, message);
     }
 
-    public void ConnecButtonCallback()
-    {
+    public void LoginConnect() {
         string brokerUri = brokerUriText.GetComponent<TMP_Text>().text.Trim().Replace("\u200B", "");
         string username = usernameText.GetComponent<TMP_Text>().text.Trim().Replace("\u200B", "");
         string password = passwordText.GetComponent<TMP_Text>().text.Trim().Replace("\u200B", "");
 
 
-        brokerUri = "io.adafruit.com";
-        username = "thanhtoan1742";
-        password = "aio_eVUG43XAwpj0GRPVkeSfvbvs4JTU";
+        brokerUri = "mqttserver.tk";
+        brokerUri = "mqttserveraaa.tk";
+        username = "bkiot";
+        password = "12345678";
 
         mqttClientBehavior.brokerAddress = brokerUri;
-        // mqttClientBehavior.brokerPort = 1883;
+        mqttClientBehavior.brokerPort = 1883;
         mqttClientBehavior.mqttUserName = username;
         mqttClientBehavior.mqttPassword = password;
 
         mqttClientBehavior.Connect();
+    }
+
+    public void LoginErrorBack() {
+        error.SetActive(false);
+        loginForm.SetActive(true);
+    }
+
+    public void mqttOnConnectionSucceeded() {
+        Debug.Log("MQTT Connection Succeeded");
         SceneManager.LoadScene("Scenes/Dashboard");
+    }
+
+    public void mqttOnConnectionFailed() {
+        Debug.Log("MQTT Connection Failed");
+        loginForm.SetActive(false);
+        error.SetActive(true);
     }
 }
--- moblile/Assets/Scripts/MqttClientBehavior.cs
+++ moblile/Assets/Scripts/MqttClientBehavior.cs
@@ -10,96 +10,55 @@ public class MqttClientBehavior : M2MqttUnityClient
 {
     // every topics that is used have to be put in here first.
     protected List<string> topics = new List<string>{
-        "thanhtoan1742/feeds/baal.temperature",
-        "thanhtoan1742/feeds/baal.humidity",
+        LedDataModel.mqttTopic,
+        PumpDataModel.mqttTopic,
+        StatusDataModel.mqttTopic,
     };
-    protected Dictionary<string, List<Action<string>>> subscribedTopicCallbacks = new Dictionary<string, List<Action<string>>>();
+    protected Dictionary<string, List<Action<string>>> listeners = new Dictionary<string, List<Action<string>>>();
 
-    public void AddSubscribedTopicCallbacks(string topic, Action<string> callback)
-    {
+    public void AddListenter(string topic, Action<string> listener) {
         Debug.Assert(topics.Contains(topic), $"{topic} not decleared in MainManager");
-
-        if (!subscribedTopicCallbacks.ContainsKey(topic))
-            subscribedTopicCallbacks[topic] = new List<Action<string>>();
-        subscribedTopicCallbacks[topic].Add(callback);
+        listeners[topic].Add(listener);
     }
 
-    public void Publish(string topic, string message)
-    {
+    public void Publish(string topic, string message) {
         client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
     }
 
-    protected override void SubscribeTopics()
-    {
-        foreach (string topic in topics)
-        {
+    protected override void SubscribeTopics() {
+        foreach (string topic in topics) {
             client.Subscribe(new string[] {topic}, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
         }
     }
 
-    protected override void UnsubscribeTopics()
-    {
-        foreach (string topic in topics)
-        {
+    protected override void UnsubscribeTopics() {
+        foreach (string topic in topics) {
             client.Unsubscribe(new string[] {topic});
         }
     }
 
-    protected override void OnConnecting()
-    {
-        base.OnConnecting();
-        Debug.Log("Connecting to broker on " + brokerAddress + ":" + brokerPort.ToString() + "...\n");
-    }
-
-    protected override void OnConnected()
-    {
-        base.OnConnected();
-        Debug.Log("Connected to broker on " + brokerAddress + "\n");
-    }
-
-    protected override void OnConnectionFailed(string errorMessage)
-    {
+    protected override void OnConnectionFailed(string errorMessage) {
         Debug.Log("CONNECTION FAILED! " + errorMessage);
     }
 
-    protected override void OnDisconnected()
-    {
-        Debug.Log("Disconnected.");
-    }
-
-    protected override void OnConnectionLost()
-    {
-        Debug.Log("CONNECTION LOST!");
-    }
-
-
-    protected override void DecodeMessage(string topic, byte[] message)
-    {
+    protected override void DecodeMessage(string topic, byte[] message) {
         string msg = System.Text.Encoding.UTF8.GetString(message);
-        foreach (Action<string> action in subscribedTopicCallbacks[topic])
-        {
+        Debug.Assert(topics.Contains(topic), $"{topic} not decleared in MainManager");
+        Debug.Log($"DECODE MESSAGE:: {topic}: {msg}");
+        foreach (Action<string> action in listeners[topic]) {
             action(msg);
         }
     }
 
-
-    private void OnDestroy()
-    {
+    private void OnDestroy() {
         Disconnect();
     }
 
 
-    private void OnValidate()
-    {
-    }
-
-    protected override void Start()
-    {
-        base.Start();
-    }
-
-    protected override void Update()
-    {
-        base.Update();
+    protected override void Awake() {
+        base.Awake();
+        timeoutOnConnection = 5000; // 5s time out
+        foreach(string topic in topics)
+            listeners[topic] = new List<Action<string>>();
     }
 }
